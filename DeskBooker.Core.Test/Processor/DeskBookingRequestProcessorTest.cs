261091074431eef502ebcd0d87bc3721cebea72e
Mateusz <glowa.mateusz@gmail.com>
2021-08-07 16:55:50 +0200

    Add project files.

---
+++ DeskBooker.Core.Test/Processor/DeskBookingRequestProcessorTest.cs
@@ -0,0 +1,37 @@
+﻿using DeskBooker.Core.Domain;
+using System;
+using Xunit;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessorTest
+    {
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            //Arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Mateusz",
+                LastName = "Glowa",
+                Email = "glowa.mateusz@gmail.com",
+                Date = new DateTime(2020, 1, 28)
+            };
+
+            var processor = new DeskBookingRequestProcessor();
+
+            //Act
+            DeskBookingResult result = processor.BookDesk(request);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+
+        }
+
+        public void 
+    }
+}
